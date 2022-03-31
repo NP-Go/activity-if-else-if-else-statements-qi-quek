3a54ac469d8cc56c978dd782580fb9ad97ca9eb7
narwhal04 <q.quek7@gmail.com>
2022-03-31 12:06:19 +0800

    working code

--- userAuthentication.go
+++ userAuthentication.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
+var message, user string
+
 func main() {
 	//Insert Code here
+
+	fmt.Printf("Input Username\n")
+	fmt.Scan(&user)
+
+	if strings.ToLower(user) == "admin" {
+		message = "Welcome, Admin!"
+	} else if strings.ToLower(user) == "robin" || strings.ToLower(user) == "john" {
+		message = "Welcome! " + user
+	} else {
+		message = "Merry Men"
+	}
+	println(message)
 }
